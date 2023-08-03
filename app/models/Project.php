758b0957e5d2e476c9df7a883ee53dc68b3edab2
Raphael Cabral <brcmesquita@gmail.com>
2023-08-02 22:47:23 -0300

    deleted all project

--- app/models/Project.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class Project extends Model
-{
-  // Implement project-related database operations here
-}
--- app/models/Task.php
+++
@@ -1,5 +0,0 @@
-namespace App\Models;
-
-class Task extends Model {
-// Implement task-related database operations here
-}
